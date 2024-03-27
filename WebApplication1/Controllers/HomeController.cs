e3cd84eef81ec554b538f16ca9421d838af80a79
Rainerre Galendez <rainerregalendez@outlook.com>
2024-03-27 23:32:09 +0800

    Added Datatable in Script

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,22 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebApplication1.Context;
 using WebApplication1.Models;
+using WebApplication1.Models.CustomModel;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        #region MainDashBoard
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+
+            var model = new HomeViewModel
+            {
+                GenreList = await GetAllItems<Genre>(),
+                MovieList = await GetAllItems<Movies>(),
+                PersonList = await GetAllItems<Persons>()
+
+            };
+            return View(model);
         }
+
+
+        private async Task<List<T>> GetAllItems<T>() where T : class
+        {
+            try
+            {
+                List<T> items = null;
+
+                if (typeof(T) == typeof(Genre))
+                {
+                    items = await _context.genres.ToListAsync() as List<T>;
+                }
+                else if (typeof(T) == typeof(Movies))
+                {
+                    items = await _context.movies.Where(m => m.IsAvailanble == true).ToListAsync() as List<T>;
+                }
+                else if(typeof(T) == typeof(Persons))
+                {
+                    items = await _context.persons.OrderBy(p => p.IsActive).ToListAsync() as List<T>;
+                }
+                else
+                {
+                    throw new NotSupportedException($"Type {typeof(T).Name} is not supported.");
+                }
+
+                return items ?? new List<T>();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error in GetAllItemsAsync<{typeof(T).Name}>: {ex.Message}");
+                throw;
+            }
+        }
+
+
+
+        #endregion
 
         public IActionResult Privacy()
         {
---
+++ WebApplication1/Models/CustomModel/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models.CustomModel
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+            Genre = new Genre();
+            Movies = new Movies();
+            Persons = new Persons();
+        }
+
+        public Genre Genre { get; set; }
+        public Movies Movies { get; set; }
+        public Persons Persons { get; set; }
+
+
+        public List<Genre> GenreList { get; set; }
+        public List<Movies> MovieList { get; set; }
+        public List<Persons> PersonList { get; set; }
+
+    }
+}
